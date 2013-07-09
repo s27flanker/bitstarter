aa7f7f2713b4d5bd89af7c4a395010fc74b828cd
p <p@ubuntu.(none)>
2013-07-09 14:25:14 -0700

    update web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var k = fs.readFileSync('index.html');
+  var k = fs.readFileSync('index.html','utf8');
   response.send(k);
   //response.send('Hello World3!');
 });
